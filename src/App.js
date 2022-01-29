8def2deb7552e69cd10be0f009ebc66010e617c3
DanWizard <tortortorented@gmail.com>
2022-01-30 01:27:26 +0200

    current

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { DataProvider } from "./context/dataContext";
+
+import axios from "axios";
+
+const nameMap = {
+  unique_tokens: "# of Unique Tokens Streamed",
+  streams_created: "# Total Streams Created",
+  active_streams: "# Active Streams",
+  value_sent: "Total Value Sent",
+  value_locked: "Total Value Locked",
+};
 
 function App() {
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      if (!data) {
+        console.log("yo");
+        const res = await axios.get("http://localhost:8080/query");
+        console.log(res.data);
+        setData(res.data.data);
+      }
+    })();
+  });
+  const credentials = {
+    data,
+    setData,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <DataProvider value={credentials}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "30px",
+          marginTop: "20px",
+        }}
+      >
+        <div>Streamflow Metrics</div>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "30px",
+        }}
+      >
+        <div
+          style={{
+            width: "500px",
+            height: "400px",
+            border: "2px solid black",
+            boxShadow: "-7px 7px 0px 0px rgba(0,0,0,1)",
+            padding: "30px",
+            fontSize: "20px",
+            display: "flex",
+            // alignItems: "center",
+            // justifyContent: "center",
+            flexDirection: "column",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {data &&
+            data.map((e, i) => {
+              return (
+                <div
+                  style={{
+                    width: "100%",
+                    display: "flex",
+                    marginTop: i !== 0 && "60px",
+                  }}
+                >
+                  <div style={{ width: "375px" }}>{nameMap[e.target]}</div>
+                  <div>{parseInt(e.datapoints[0])}</div>
+                </div>
+              );
+            })}
+          {/* <div style={{ width: "100%", display: "flex", marginTop: "10px" }}>
+            <div style={{ width: "250px" }}>Total Value Sent:</div>
+            <div>20,000,000</div>
+          </div> */}
+        </div>
+      </div>
+    </DataProvider>
   );
 }
 
